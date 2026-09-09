12fade341154f6105db1c472938e6a364a0258ef
AkashPatel6 <Akash.Patel6@cognizant.com>
2026-09-09 15:25:28 +0530

    Revert "conversation class"

--- backend/src/main/java/com/mytutor/repository/ConversationRepository.java
+++ backend/src/main/java/com/mytutor/repository/ConversationRepository.java
@@ -3,5 +3,5 @@ import com.mytutor.entity.Conversation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 public interface ConversationRepository extends JpaRepository<Conversation,Integer>{
-    List<Conversation> findTop20ByUser_UserIdOrderByCreatedDateDesc(String userId);
+
 }
