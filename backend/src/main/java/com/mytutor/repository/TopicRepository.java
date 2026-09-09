19a6b29bffb198a45adda81aadd5f8547d537805
Lahari <2521729@cognizant.com>
2026-09-09 15:16:09 +0530

    Implement topic management

--- backend/src/main/java/com/mytutor/repository/TopicRepository.java
+++ backend/src/main/java/com/mytutor/repository/TopicRepository.java
@@ -3,5 +3,6 @@ import com.mytutor.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 public interface TopicRepository extends JpaRepository<Topic,Integer>{
-
+    Optional<Topic> findFirstByTopicNameIgnoreCase(String name);
+    List<Topic> findAllByOrderByTopicNameAsc();
 }
