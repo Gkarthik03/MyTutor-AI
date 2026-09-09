51f1610459859719c4e9bd2800db8cb874a711f9
Sowmya <2521744@cognizant.com>
2026-09-09 15:50:07 +0530

    Implement topic management

--- backend/src/main/java/com/mytutor/repository/KnowledgeRepositoryRepository.java
+++ backend/src/main/java/com/mytutor/repository/KnowledgeRepositoryRepository.java
@@ -3,5 +3,5 @@ import com.mytutor.entity.KnowledgeRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 public interface KnowledgeRepositoryRepository extends JpaRepository<KnowledgeRepository,Integer>{
-
+    List<KnowledgeRepository> findByTopic_TopicId(Integer topicId);
 }
